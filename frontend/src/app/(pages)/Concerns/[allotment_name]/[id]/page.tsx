fac495d7c1bc8acfeee66318292b046e599ea861
pockerpredator <pockerpredator>
2025-07-04 19:01:28 +0800

    dependency chages n [id]/page.tsx

--- frontend/src/app/(pages)/Concerns/[allotment_name]/[id]/page.tsx
+++ frontend/src/app/(pages)/Concerns/[allotment_name]/[id]/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 import React from 'react';
-import Header from '../components/Header';
-import Breadcrumbs from '../../../../../components/Navigation/BreadCrumbs';
-import Concerns from "../components/Concern"
-import SideBar from "../../components/SideBar"
-import ReplyBox from '../components/Reply'; 
+import Header from './components/Header';
+import Breadcrumbs from '../../../../components/Navigation/BreadCrumbs';
+import Concerns from "./components/Concern"
+import SideBar from "../components/SideBar"
+import ReplyBox from './components/Reply'; 
 
 export default function TestConcern() {
   return (
