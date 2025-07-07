c132bd9954aad91eb164fdd7a7fa9d6ef116e658
KevanSoon <64390917+KevanSoon@users.noreply.github.com>
2025-07-07 15:59:38 +0800

    Adjusted Concerns page to have mobile view without affecting desktop view

--- frontend/src/app/(pages)/Concerns/[allotment_name]/page.tsx
+++ frontend/src/app/(pages)/Concerns/[allotment_name]/page.tsx
@@ -5,7 +5,7 @@ import { useParams, notFound } from 'next/navigation';
 import Header from './components/Header';
 import Breadcrumbs from '../../../components/Navigation/BreadCrumbs';
 import Concerns from "./components/Concerns";
-import SideBar from "../components/SideBar"
+import SideBar from "../components/SideBar";
 
 // used temporarily until backend is linked
 const validAllotments = ['Plantation%20Acres', 'Tengah%20Community%20Club', 'Garden%20Vale','Plantation%20Grove'];
@@ -13,9 +13,9 @@ const validAllotments = ['Plantation%20Acres', 'Tengah%20Community%20Club', 'Gar
 export default function TestConcern() {
   const { allotment_name } = useParams() as { allotment_name: string };
 
-if (!validAllotments.includes(allotment_name as string)) {
+  if (!validAllotments.includes(allotment_name as string)) {
     notFound();
-    }
+  }
 
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-start bg-white">
@@ -23,9 +23,10 @@ if (!validAllotments.includes(allotment_name as string)) {
         {/* In the future, might be better to use Breadcrumbs=allotment_name */}
         <Breadcrumbs />
         <Header />
-        <div className="flex gap-12">
-          <div className="w-full md:w-3/4 lg:w-4/5 pl-16">
-            <Concerns allotmentName={allotment_name}/>
+        
+        <div className="flex flex-col md:flex-row gap-4 md:gap-12 px-4 md:px-0 mt-4">
+          <div className="w-full md:w-3/4 lg:w-4/5 md:pl-16">
+            <Concerns allotmentName={allotment_name} />
           </div>
           <div className="w-full md:w-1/4 lg:w-1/5">
             <SideBar />
